d736ec358add3e439f95246430f21d58b9715653
sladoy <40322600+sladoy@users.noreply.github.com>
2018-06-27 19:02:46 +0200

    Passing Value to Label
    I need to find better solution.

--- GUI.py
+++ GUI.py
@@ -1,10 +1,11 @@
-from tkinter import Tk, Entry, Label, Button
+from tkinter import Tk, Entry, Label, Button, StringVar
 from Reader import RSS
 
 
 class MainGUI:
     def __init__(self, root):
         self.root = root
+        self.V = StringVar()
         self.label = Label(root, text='Wprowadz link RSS')
         self.label.pack()
         self.entry = Entry(root)
@@ -13,23 +14,25 @@ class MainGUI:
         self.start_button.pack()
         self.show_more_button = Button(root, text='Show more about')
         self.show_more_button.pack()
-        self.data_label = Label(root)
+        self.data_label = Label(root, textvariable=self.V)
         self.data_label.pack()
         self.url = 0
 
     def start(self):
+        #Uzyć Tree View do wpisywania wartosci
+        value_list = ''' '''
         self.url = RSS(self.entry.get())
         positions = self.url.show_titles()
-        print(self.url)
 
         for x, y in positions.items():
-            print(x + '\t link: ' + y)
+            value_list += x + '\t link: ' + y + '\n'
+
+        self.V.set(value_list)
 
 
 if __name__ == '__main__':
     root = Tk()
     root.bind('<Escape>', quit)
     root.title('RSS Reader')
-    root.geometry('400x200')
     MainGUI(root)
     root.mainloop()
